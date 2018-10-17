e13d79fd32000f6a983438ce48a93b9179cf8d9d
Jia Rong <jr.loves.maths@gmail.com>
2018-10-18 00:21:46 +0800

    feat: increased patch rate

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const playerMoveSpeed = 200 / 1000
-// const patchRate = 16.6666
-const patchRate = 50
+const patchRate = 16.6666
+// const patchRate = 50
 // update slightly faster than the patchRate
 const updateRate = patchRate - 1
 const playerDt = playerMoveSpeed * updateRate
